6f64f7e13e0a52f430305d27133a83f4a156c50f
Lê Sỹ Tuyền <hatohuidev@gmail.com>
2026-02-07 00:26:26 +0700

    refactor: clean up imports and improve tsconfig formatting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import registerGSAPPlugins from './config/registerGSAPPlugins'
 import { RouterProvider } from 'react-router'
 import router from './config/dynamicRouter'
-import './config/i18n'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 registerGSAPPlugins()
