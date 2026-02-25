da29f41931875fd740d5e72449bf5cc764118128
Lê Sỹ Tuyền <hatohuidev@gmail.com>
2026-02-25 15:22:58 +0700

    feat: add Grafana CloudWatch policy and integrate with API Gateway

--- infra/modules/iam/policy.tf
+++ infra/modules/iam/policy.tf
@@ -1,3 +1,63 @@
+resource "aws_iam_policy" "grafana_cloudwatch" {
+  name        = "${var.project_name}-grafana-cloudwatch"
+  description = "Policy for Grafana to read CloudWatch metrics and logs"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "CloudWatchMetrics"
+        Effect = "Allow"
+        Action = [
+          "cloudwatch:DescribeAlarmsForMetric",
+          "cloudwatch:DescribeAlarmHistory",
+          "cloudwatch:DescribeAlarms",
+          "cloudwatch:ListMetrics",
+          "cloudwatch:GetMetricData",
+          "cloudwatch:GetMetricStatistics",
+          "cloudwatch:GetInsightRuleReport"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "CloudWatchLogs"
+        Effect = "Allow"
+        Action = [
+          "logs:DescribeLogGroups",
+          "logs:GetLogGroupFields",
+          "logs:StartQuery",
+          "logs:StopQuery",
+          "logs:GetQueryResults",
+          "logs:GetLogEvents"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "EC2TagsAndRegions"
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeTags",
+          "ec2:DescribeInstances",
+          "ec2:DescribeRegions"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "TagGetResources"
+        Effect = "Allow"
+        Action = [
+          "tag:GetResources"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project_name}-grafana-cloudwatch"
+  }
+}
+
 resource "aws_iam_policy" "github_actions_ecr" {
   name        = "${var.project_name}-github-actions-ecr"
   description = "Policy for GitHub Actions to push/pull images to/from ECR and update Lambda functions"
